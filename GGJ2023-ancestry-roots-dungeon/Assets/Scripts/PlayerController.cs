84d1ec9fef33152eaa7aa084ea11630c150c8166
molenubar <molenubar@hotmail.com>
2023-02-04 15:34:30 -0800

    physics introduced to the player controller

--- GGJ2023-ancestry-roots-dungeon/Assets/Scripts/PlayerController.cs
+++ GGJ2023-ancestry-roots-dungeon/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D player;
+    public Rigidbody2D body;
     Vector2 moveDirection = Vector2.zero;
-    public float speed = 1;
+    public float moveSpeed = 1;
 
     public bool IsAllowInput = true;
+    public float rotationSpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,25 +28,61 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (IsAllowInput)
+        if (!IsAllowInput && body.velocity != Vector2.zero)
         {
-            float moveX = Input.GetAxis("Horizontal");
-            float moveY = Input.GetAxis("Vertical");
-            moveDirection = new Vector2(moveX, moveY).normalized;
-            //player.Cast(moveDirection, )
-            var parent = player.transform.parent;
-            parent.transform.Translate(moveDirection * speed * Time.deltaTime);
-            if (Input.GetKey(KeyCode.Q))
-            {
-                parent.transform.Rotate(new Vector3(0, 0, 1));
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                parent.transform.Rotate(new Vector3(0, 0, -1));
-            }
-
-
+            //moveSpeed = 0;
+            body.velocity = Vector2.zero;
         }
+        //else
+        //{
+        //    moveSpeed = 1;
+        //}
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //movement.x = (-transform.right * Time.deltaTime * (moveX * -moveSpeed)).x;
+            movement.x = moveX * moveSpeed;
+            //Debug.Log("KeyPressA: X: " + moveX + " Y: " + moveY);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //movement.x = (transform.right * Time.deltaTime * (moveX * moveSpeed)).x;
+            movement.x = moveX * moveSpeed;
+            //Debug.Log("KeyPressD: X: " + moveX + " Y: " + moveY);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            //movement.y = (transform.up * Time.deltaTime * (moveY * moveSpeed)).y;
+            movement.y = moveY * moveSpeed;
+            //Debug.Log("KeyPressW: X: " + moveX + " Y: " + moveY);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //movement.y = (transform.up * Time.deltaTime * (moveY * moveSpeed)).y;
+            movement.y = moveY * moveSpeed;
+            //Debug.Log("KeyPressS: X: " + moveX + " Y: " + moveY);
+        }
+
+        //movement = movement + (Vector2)(transform.position);
+
+        //body.AddForce(movement);
+        body.velocity = (movement);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            body.transform.Rotate(new Vector3(0, 0, rotationSpeed));
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            body.transform.Rotate(new Vector3(0, 0, -rotationSpeed));
+        }
+
+
     }
 
     void Move(Vector2 direction)
