36701f3fdf34776a2ca25efe4f3edf9de87dd979
Josh Kabo <joshkong1@gmail.com>
2023-02-05 10:56:36 -0800

    More sfx, root of integration

--- GGJ2023-ancestry-roots-dungeon/Assets/Scripts/AttackManager.cs
+++ GGJ2023-ancestry-roots-dungeon/Assets/Scripts/AttackManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackManager : MonoBehaviour {
-
+public class AttackManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    // public AudioClip 
     static private AttackManager instance;
-    static public AttackManager GetInstance() {
-        if (null == instance) {
+    static public AttackManager GetInstance()
+    {
+        if (null == instance)
+        {
             Debug.Log("AttackManager.GetInstance() called before init");
         }
         return instance;
@@ -16,20 +20,25 @@ public class AttackManager : MonoBehaviour {
 
     public GameObject fireBall = null;
 
-    public enum AttackMode {
+    public enum AttackMode
+    {
         None,
         Fireball
     }
 
-    void Awake() {
+    void Awake()
+    {
         instance = this;
+        audioSource = GetComponent<AudioSource>();
     }
 
     public AttackMode currentAttackMode = AttackMode.Fireball;
 
-    public void fire(Vector2 direction) {
+    public void fire(Vector2 direction)
+    {
         Debug.Log(currentAttackMode);
-        switch (currentAttackMode) {
+        switch (currentAttackMode)
+        {
             case (AttackMode.None):
                 break;
             case (AttackMode.Fireball):
@@ -44,15 +53,18 @@ public class AttackManager : MonoBehaviour {
 
 
     }
-    public void UpdatePlayerHealthUI() {
+    public void UpdatePlayerHealthUI()
+    {
 
     }
 
-    public void collideAttackingEntities(GameObject a, GameObject b) {
+    public void collideAttackingEntities(GameObject a, GameObject b)
+    {
         Debug.Log("Exchanging damages between " + a.tag + " & " + b.tag + "?");
         var aeA = a.GetComponent<AttackEntity>();
         var aeB = b.GetComponent<AttackEntity>();
-        if (null == aeA || null == aeB) {
+        if (null == aeA || null == aeB)
+        {
             // Either one of those isn't meant to attach/be attacked
             Debug.Log("No need to exchange damage");
             return;
